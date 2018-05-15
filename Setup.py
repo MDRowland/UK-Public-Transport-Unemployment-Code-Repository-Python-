77611d43e5410fffbb3c73a189234ef9633823f6
MDRowland <39090630+MDRowland@users.noreply.github.com>
2018-05-15 15:33:38 +0100

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,5 +1,5 @@
 import openpyxl as opxl
-import os, zipfile
+import os
 ####################################
 ####### Generic Functions ##########
 def file_len(fname):              ##
@@ -9,8 +9,7 @@ def file_len(fname):              ##
     return i + 1                  ##
 ####################################
 ####### Files and other ############
-conversion_file = "ExcelSheets/SCR_LSOA_Estimates.xlsx"
-wb = opxl.load_workbook(conversion_file, read_only = True)
+
 print('Performing first-time setup.')
 if os.path.exists("Output Files"):
     pass
@@ -22,19 +21,19 @@ if os.path.exists("Programme Files"):
     pass
 else:
     os.makedirs("Programme Files")
-    f = open("Programme Files/z.txt")
+    f = open("Programme Files/z.txt", "w")
     f.write("0 0")
     f.close()
-    f = open("Programme Files/CostCoefficients.txt")
+    f = open("Programme Files/CostCoefficients.txt", "w")
     f.write("{'Time': [0, 1, 0, 0, 0, 0], 'Disabled': [0.1, 1, 0, 0.3, 0.1, 0.3], 'Time + Living Wage': [459.77, 1, 0, 0, 0, 0], 'Cycling': [0, 0, 1, 0, 0, 0]}")
     f.close()
-    f = open("Programme Files/CostCoefficients_MetaData.txt")
+    f = open("Programme Files/CostCoefficients_MetaData.txt", "w")
     f.write("Name Cost_Coefficent Time_Coefficent Distance_Coefficent Changes_malus walking_time_malus walking_distance_malus")
     f.close()
-    f = open("Programme Files/MaxCost.txt")
+    f = open("Programme Files/MaxCost.txt", "w")
     f.write("{'Twenty Minutes': 1200, 'One Hour': 3600, 'Ten Miles': '16000'}")
     f.close()
-    f = open("Programme Files/TargetJobCodes.txt")
+    f = open("Programme Files/TargetJobCodes.txt", "w")
     f.write("[927,622,613,923,711,624,924,543,913,911]")
     f.close()
 if os.path.exists("Locality Files"):
@@ -44,13 +43,12 @@ else:
 if os.path.exists("ExcelSheets"):
     pass
 else:
-    os.makedirs("ExcelSheets")   
-    zip_ref = zipfile.ZipFile("ExcelSheets.7z", 'r')
-    zip_ref.extractall("ExcelSheets")
-    zip_ref.close()
+    os.makedirs("ExcelSheets")
+    for sheet in ["SCR_GeoCode_DeprivationFraction","SCR_LSOA_Estimates","SCR_LSOA_Workplace_Totals","SCR_LSOAConversions","SCR_MSOA_Workplace_Data_Split"]:
+        os.rename(sheet,str("ExcelSheets/"+sheet))
     
-
-
+conversion_file = "ExcelSheets/SCR_LSOA_Estimates.xlsx"
+wb = opxl.load_workbook(conversion_file, read_only = True)
 
 print("Identifying numbers of LSOAs and MSOAs.")
 LSOA_dictionary = {}
@@ -75,8 +73,4 @@ f =open("Programme Files/MSOA_Dictionary.txt", "w")
 f.write(str(MSOA_dictionary))
 f.close()
 print('LSOAs counted.')
-
-    
-
 print('Setup complete!')
-
